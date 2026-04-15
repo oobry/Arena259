c355d3df105e8fcf9a26fbcac42a3eaf5527510f
Eldon Pouliot <pouliote@sou.edu>
2026-04-15 16:46:28 -0700

    Added giant leech cpp and header file

---
+++ src/creatures/giantleech.cpp
@@ -0,0 +1,8 @@
+#include "giantleech.h"
+
+// Oz and Eldon
+
+Creature makeGiantleech() {
+    Creature leech = Creature("Giant Leech", 100, 18);
+    return leech;
+}
---
+++ src/creatures/giantleech.h
@@ -0,0 +1,9 @@
+#ifndef GIANTLEECH_H
+#define GIANTLEECH_H
+
+#include "../creature.h"
+
+
+Creature makeGiantleech();
+
+#endif
