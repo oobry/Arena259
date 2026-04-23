2b1f91e10a8807da8345fdb41d6a0c63bc06bba3
grumpywizardstudent <klein@sou.edu>
2026-04-22 17:09:15 -0700

    adds comments to arenarand.h and arenarand.cpp

--- src/arenarand.cpp
+++ src/arenarand.cpp
@@ -1,6 +1,8 @@
 #include "arenarand.h"
 
-
+// simulates a coin flip and returns the result as a bool
 bool ArenaRand::flipCoin() { return COIN(gen); }
 
+// pass in two ints to define a range of values and return
+// a random value from within that range.
 int ArenaRand::randomValue(int min, int max) { return std::uniform_int_distribution<>{min, max}(gen); }
--- src/arenarand.h
+++ src/arenarand.h
@@ -6,12 +6,12 @@
 
 class ArenaRand {
     public:
-        static bool flipCoin();
-        static int randomValue(int min, int max);
+        static bool flipCoin(); // flip a coin and get a bool
+        static int randomValue(int min, int max); // returns a random int from within the range
     private:
-        inline static std::random_device rd{};
-        inline static std::mt19937 gen{rd()};
-        inline static std::uniform_int_distribution<> COIN{0,1};
+        inline static std::random_device rd{}; // creates a unique seed value for random number generation
+        inline static std::mt19937 gen{rd()}; // the classic mersenne twist
+        inline static std::uniform_int_distribution<> COIN{0,1}; // creates a uniform distribution for flipCoin()
 };
 
 #endif
